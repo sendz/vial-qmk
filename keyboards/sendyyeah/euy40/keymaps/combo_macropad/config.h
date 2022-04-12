c73d93cbb44600ddc48b2ccf7b3f637fb92e8671
Sendy Aditya Suryana <sendzation@gmail.com>
2022-04-12 12:41:20 +0700

    Add euy40 keyboard

---
+++ keyboards/sendyyeah/euy40/keymaps/combo_macropad/config.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+/* Split hand configration */
+#define SPLIT_HAND_MATRIX_GRID D4, F4
+#define SPLIT_HAND_MATRIX_GRID_LOW_IS_RIGHT
+
+#define VIAL_KEYBOARD_UID {0xBB, 0x25, 0x78, 0x78, 0xFD, 0x38, 0xB3, 0x6D}
+
+#define VIAL_ENCODER_DEFAULT { KC_VOLU, KC_VOLD, KC_PGUP, KC_PGDOWN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS }
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
---
+++ keyboards/sendyyeah/euy40/rules.mk
@@ -0,0 +1,10 @@
+# MCU name
+MCU = atmega32u4
+
+# Bootloader selection
+BOOTLOADER = caterina
+
+EXTRAKEY_ENABLE = yes    # Audio control and System control
+SPLIT_KEYBOARD = yes
+LTO_ENABLE = yes
+ENCODER_ENABLE = yes
