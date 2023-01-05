d3980a4ce23785c9939fa69d4191a5b251e89b5a
sendz <sendzation@gmail.com>
2023-01-05 17:46:55 +0700

    Add cacah to KBDKSP

---
+++ keyboards/kbdksp/cacah/keymaps/vial/config.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xC6, 0xED, 0xDB, 0x0C, 0x58, 0x33, 0x3F, 0x0C}
+
+#define VIAL_ENCODER_DEFAULT { \
+    KC_VOLU, KC_VOLD, \
+    KC_TRNS, KC_TRNS, \
+    KC_TRNS, KC_TRNS, \
+    KC_TRNS, KC_TRNS, \
+}
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 4
