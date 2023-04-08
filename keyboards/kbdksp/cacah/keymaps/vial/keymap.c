0388f760286bac937567794dead439fddd9ca84c
Sendy <sendzation@gmail.com>
2023-04-08 09:32:42 +0700

    Default Numpad

--- keyboards/kbdksp/cacah/keymaps/vial/keymap.c
+++ keyboards/kbdksp/cacah/keymaps/vial/keymap.c
@@ -18,7 +18,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_ortho(
-      KC_NLCK, KC_PSLS, KC_PAST, KC_PMNS,
+      LT(1, KC_NLCK), KC_PSLS, KC_PAST, KC_PMNS,
       KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
       KC_P4,   KC_P5,   KC_P6,   KC_NUBS,
       KC_P1,   KC_P2,   KC_P3,   KC_PENT,
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______,
     _______, _______, _______, _______,
     _______, _______, _______, _______,
-    _______, _______, _______, _______
+    _______, _______, QK_BOOT, _______
   ),
   [2] = LAYOUT_ortho(
     _______, _______, _______, _______,
@@ -49,7 +49,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 #if defined(ENCODER_MAP_ENABLE)
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] =   { ENCODER_CCW_CW(KC_VOLU, KC_VOLD)           },
+    [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU)           },
     [1] =   { ENCODER_CCW_CW(RGB_HUD, RGB_HUI)           },
     [2] =   { ENCODER_CCW_CW(RGB_VAD, RGB_VAI)           },
     [3] =   { ENCODER_CCW_CW(RGB_RMOD, RGB_MOD)          },
