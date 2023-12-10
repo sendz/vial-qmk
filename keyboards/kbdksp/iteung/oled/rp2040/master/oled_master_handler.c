7fbe44e45a5fae14d53aad5f63ccfcc7573860cf
Sendy Aditya Suryana <sendzation@gmail.com>
2023-12-10 10:52:44 +0700

    Rotate OLED master to portrait mode

--- keyboards/kbdksp/iteung/oled/rp2040/master/oled_master_handler.c
+++ keyboards/kbdksp/iteung/oled/rp2040/master/oled_master_handler.c
@@ -8,24 +8,24 @@ static const char PROGMEM ICON_LAYER[] = {0x80, 0x81, 0x82, 0x83, 0};
 void render_master_oled(void) {
     oled_on();
 
-    oled_write_P(ICON_LAYER, false);
+    oled_write_ln(ICON_LAYER, false);
 
     switch (get_highest_layer(layer_state)) {
         case 0:
-            oled_write_P(PSTR("QWERTY"), false);
+            oled_write_P(PSTR("QWERT"), false);
         break;
         case 1:
-            oled_write_P(PSTR("LOWER "), false);
+            oled_write_P(PSTR("LOWER"), false);
         break;
         case 2:
-            oled_write_P(PSTR("RAISE "), false);
+            oled_write_P(PSTR("RAISE"), false);
         break;
         case 3:
-            oled_write_P(PSTR("ADJUST"), false);
+            oled_write_P(PSTR("ADJST"), false);
         break;
         default:
-            oled_write_P(PSTR("UNDEF "), false);
+            oled_write_P(PSTR("UNDEF"), false);
     }
 }
 
-oled_rotation_t rotate_master(oled_rotation_t rotation) {return OLED_ROTATION_180;}
+oled_rotation_t rotate_master(oled_rotation_t rotation) {return OLED_ROTATION_270;}
