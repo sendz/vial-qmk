fe6bbaf479dd449cc55938cef1d0a5cc6b84fe5b
sendz <sendzation@gmail.com>
2024-01-04 05:39:21 +0700

    VIAL Insecure and enable OLED

--- keyboards/crkbd/keymaps/vial/rules.mk
+++ keyboards/crkbd/keymaps/vial/rules.mk
@@ -1,13 +1,15 @@
 VIA_ENABLE          = yes
 VIAL_ENABLE         = yes
 LTO_ENABLE          = yes
+VIAL_INSECURE		= yes
 
 RGBLIGHT_ENABLE     = yes
 RGB_MATRIX_ENABLE   = no # Can't have RGBLIGHT and RGB_MATRIX at the same time.
 MOUSEKEY_ENABLE     = no
-OLED_ENABLE         = no
-OLED_DRIVER         = SSD1306
+OLED_ENABLE         = yes
+OLED_DRIVER         = ssd1306
 EXTRAKEY_ENABLE     = yes
 COMBO_ENABLE        = no
 
 QMK_SETTINGS        = no
+KEY_OVERRIDE_ENABLE = no
