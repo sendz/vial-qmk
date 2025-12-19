d8969d4e477b54a32f01269825941d9a37669a42
sendz <sendzation@gmail.com>
2025-12-19 21:50:31 +0700

    Add scroll mode in joystick| |

--- keyboards/sendyyeah/dicotyl/keymaps/vial/keymap.c
+++ keyboards/sendyyeah/dicotyl/keymaps/vial/keymap.c
@@ -25,16 +25,16 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [_LOWER] = LAYOUT_split_4x6_1(
         KC_GRAVE,   KC_1,       KC_2,       KC_3,       KC_4,       KC_5,           KC_6,       KC_7,       KC_8,       KC_9,       KC_0,       KC_DEL,
-        _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,        XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
-        _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,        XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,
+        _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,        KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,   _______,    _______,
+        _______,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,    XXXXXXX,        KC_LBRC,    KC_RBRC,    KC_MINS,    KC_EQL,     KC_BSLS,    _______,
         _______,    _______,    _______,    _______,    _______,    _______,        _______,    _______,    _______,    _______,    _______,
                                                                     XXXXXXX
     ),
 
     [_RAISE] = LAYOUT_split_4x6_1(
         KC_GRAVE,   KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,          KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_DEL,
-        _______,    KC_F11,     KC_F12,     _______,    _______,    _______,        _______,    _______,    _______,    _______,    _______,    _______,
-        _______,    _______,    _______,    _______,    _______,    _______,        _______,    _______,    _______,    _______,    _______,    _______,
+        _______,    KC_F11,     KC_F12,     _______,    _______,    _______,        KC_LEFT,    KC_DOWN,    KC_UP,      KC_RIGHT,   _______,    _______,
+        _______,    _______,    _______,    _______,    _______,    _______,        KC_LBRC,    KC_RBRC,    KC_MINS,    KC_EQL,     KC_BSLS,    _______,
         _______,    _______,    _______,    _______,    _______,    _______,        _______,    _______,    _______,    _______,    _______,
                                                                     XXXXXXX
     ),
@@ -48,14 +48,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 };
 
+static bool scrolling_mode = false;
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+        case _RAISE:  // If we're on the _RAISE layer enable scrolling mode
+        case _LOWER:
+            scrolling_mode = true;
+            pointing_device_set_cpi(2000);
+            break;
+        default:
+            if (scrolling_mode) {  // check if we were scrolling before and set disable if so
+                scrolling_mode = false;
+                pointing_device_set_cpi(8000);
+            }
+            break;
+    }
+    return state;
+}
 
 report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
-    if (layer_state_is(_LOWER) || layer_state_is(_RAISE)) {
+    if (scrolling_mode) {
         mouse_report.h = mouse_report.x;
         mouse_report.v = mouse_report.y;
         mouse_report.x = 0;
         mouse_report.y = 0;
     }
-
     return mouse_report;
 }
